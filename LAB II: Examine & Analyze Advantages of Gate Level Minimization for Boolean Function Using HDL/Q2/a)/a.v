ae865eba7452cc630f5ff7a9982928572c66c9b7
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-14 19:44:46 +0530

    Update a.v

--- LAB II: Examine & Analyze Advantages of Gate Level Minimization for Boolean Function Using HDL/Q2/a)/a.v
+++ LAB II: Examine & Analyze Advantages of Gate Level Minimization for Boolean Function Using HDL/Q2/a)/a.v
@@ -4,4 +4,24 @@
 
 // Simplified: A = X'Z + XY
 
-// 
+// Gate-Level
+module circuit3(A, X, Y, Z)
+     input      X, Y, Z;
+     output     A;
+     wire       w1, w2, w3;
+     and        G1(w1, X, Y);
+     not        G2(w2, X);
+     and        G3(w3, w2, Z);
+     or         G4(A, w1, w3);
+endmodule
+
+// Data-Flow
+module circuit3(A, X, Y, Z)
+     input      X, Y, Z;
+     output     A;
+     wire       w1, w2, w3;
+     assign     w1 = (X && Y);
+     assign     w2 = !(X);
+     assign     w3 = (w2 && Z);
+     assign     A  = (w1 || w3);
+endmodule
