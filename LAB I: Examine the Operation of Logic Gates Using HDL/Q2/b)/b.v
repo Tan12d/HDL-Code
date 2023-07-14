a0db5414c21f5e7cffd87faca82f3067dd3e3456
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-14 13:37:57 +0530

    Create b.v

---
+++ LAB I: Examine the Operation of Logic Gates Using HDL/Q2/b)/b.v
@@ -0,0 +1,22 @@
+// Q2. Using a single 7400 IC, connect and implement a circuit using HDL that   produces
+     // b) A two-input AND. 
+
+
+
+// Gate-Level
+module And(A, B, X)
+  input  (A, B);
+  output X;
+  wires  w1;
+  nand   G1(w1, A, B);
+  nand   G2(X, w1, w1);
+endmodule
+
+// Data-Flow
+  module And(A, B, X)
+  input     (A, B);
+  output    X;
+    wires   w1;
+    assign  w1 = !(A && B);
+    assign  X  = !(w1 && w1);
+  endmodule
