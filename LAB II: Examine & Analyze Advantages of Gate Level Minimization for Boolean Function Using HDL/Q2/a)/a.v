b7d7f9568d8f20c171008726d9616abc81ba6b1d
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-14 17:47:44 +0530

    Create a.v

---
+++ LAB II: Examine & Analyze Advantages of Gate Level Minimization for Boolean Function Using HDL/Q2/a)/a.v
@@ -0,0 +1,7 @@
+// Q2. Simplify the following Boolean functions to a minimum number of literals and write HDL program for each functions.
+     // a) F= XY +X′Z+YZ
+
+
+// Simplified: A = X'Z + XY
+
+// 
