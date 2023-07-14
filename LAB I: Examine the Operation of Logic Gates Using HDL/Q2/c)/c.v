ed604d3e585dba16abd942dfe213104505b24820
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-15 01:19:57 +0530

    Update c.v

--- LAB I: Examine the Operation of Logic Gates Using HDL/Q2/c)/c.v
+++ LAB I: Examine the Operation of Logic Gates Using HDL/Q2/c)/c.v
@@ -7,7 +7,7 @@
 module Or(A, B, X)
   input  (A, B);
   output X;
-  wires  w1,w2;
+  wire   w1,w2;
   nand   G1(w1, A, A);
   nand   G2(w2, B, B);
   nand   G3(X, w1, w2);
@@ -17,7 +17,7 @@ endmodule
   module Or(A, B, X)
   input     (A, B);
   output    X;
-  wires     w1,w2;
+  wire      w1,w2;
     assign  w1 = !(A && A);
     assign  w2 = !(A && B);
     assign  X  = !(w1 && w2);
