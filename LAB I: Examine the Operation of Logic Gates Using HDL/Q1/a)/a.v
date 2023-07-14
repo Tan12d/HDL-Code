0e7dab013e38510a506a42abb53f502ccbac0d96
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-14 12:59:58 +0530

    Create a.v

---
+++ LAB I: Examine the Operation of Logic Gates Using HDL/Q1/a)/a.v
@@ -0,0 +1,17 @@
+// 1. Investigation of the logic behavior of various gates using HDL:
+ 	// a) 7400 quadruple two-input NAND gates 
+
+
+// Gate-Level
+module circuit1(A, B, X)
+  input  (A, B);
+  output X;
+  nand   G1(X, A, B);
+endmodule
+
+// Data-Flow
+  module circuit1(A, B, X)
+  input   (A, B);
+  output  X;
+    assign  X = !(A && B);
+  endmodule
